fd72f828ffcdc582f94222d6c795bb28927edc27
Tanishq Tyagi <tanishqtyagi@Tanishqs-MacBook-Air.local>
2025-06-23 01:31:34 +0530

    backend url update

---
+++ client/src/socket.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+import io from "socket.io-client";
+
+const server =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "";
+
+console.log(server);
+
+const connectionOptions = {
+  "force new connection": true,
+  reconnectionAttempts: "Infinity",
+  timeout: 10000,
+  transports: ["websocket"],
+};
+
+export const socket = io(server, connectionOptions);
