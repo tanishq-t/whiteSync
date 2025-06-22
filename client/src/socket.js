e052480df8e1db26ed8884712e9ed02d9ca7f584
Tanishq Tyagi <149141752+tanishq-t@users.noreply.github.com>
2025-06-23 02:02:33 +0530

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,10 +1,7 @@
 /* eslint-disable no-undef */
 import io from "socket.io-client";
 
-const server =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000"
-    : "https://whitesync.onrender.com/";
+const server = "https://whitesync.onrender.com";
 
 console.log(server);
 
